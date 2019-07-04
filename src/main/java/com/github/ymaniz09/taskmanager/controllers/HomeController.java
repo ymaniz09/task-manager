ff6c78a7df2e0c878ef080e741d5edd35d378d31
Ygor <ygor.maniz@gmail.com>
2019-07-03 22:01:38 -0300

    Hello world from the controller

--- src/main/java/com/github/ymaniz09/taskmanager/controllers/HomeController.java
+++ src/main/java/com/github/ymaniz09/taskmanager/controllers/HomeController.java
@@ -1,8 +1,17 @@
 package com.github.ymaniz09.taskmanager.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
-    
+
+    @GetMapping("/")
+    public ModelAndView home() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("home/home");
+        mv.addObject("message", "controller message");
+        return mv;
+    }
 }
